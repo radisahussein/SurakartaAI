f58e7c726f959be4d2e0834e8f4913fb017917f2
rainamra <56628802+rainamra@users.noreply.github.com>
2021-06-21 00:33:27 +0700

    eat piece and valid moves

--- surakarta/board.py
+++ surakarta/board.py
@@ -66,7 +66,168 @@ class Board:
         moves = {}
         return moves
 
-    
+     def draw(self, win):
+        self.draw_squares(win)
+        for row in range(ROWS):
+            for col in range(COLS):
+                piece = self.board[row][col]
+                if piece != 0:
+                    piece.draw(win)
+
+    def remove(self, piece):
+        # for piece in pieces:
+        self.board[piece.row][piece.col] = 0
+        if piece != 0:
+            if piece.color == RED:
+                self.red_left -= 1
+            else:
+                self.white_left -= 1
+
+    def winner(self):
+        if self.red_left <= 0:
+            return WHITE
+        elif self.white_left <= 0:
+            return RED
+
+        return None
+
+    def get_valid_moves(self, piece):
+        moves = {}
+        left = piece.col - 1
+        right = piece.col + 1
+        row = piece.row
+        col = piece.col
+
+        # moving up
+        moves.update(self._check_moves(row, left))
+        # moving down
+        moves.update(self._check_moves(row, right))
+        # moving left
+        moves.update(self._check_moves(row - 1, col))
+        # moving right
+        moves.update(self._check_moves(row + 1, col))
+        # moving up left
+        moves.update(self._check_moves(row - 1, left))
+        # moving up right
+        moves.update(self._check_moves(row - 1, right))
+        # moving down left
+        moves.update(self._check_moves(row + 1, left))
+        # moving down right
+        moves.update(self._check_moves(row + 1, right))
+
+        if piece.row <= 2: #check down
+            moves.update(self._check_eatable_vertical(row -1, max(piece.row-6, -1), -1, piece.col, piece.color, 0))
+            moves.update(self._check_eatable_vertical(row +1, min(piece.row+6, ROWS), 1, piece.col, piece.color, 0))
+        else: #check up
+            moves.update(self._check_eatable_vertical(row -1, max(piece.row-6, -1), -1, piece.col, piece.color, 0))
+            moves.update(self._check_eatable_vertical(row +1, min(piece.row+6, ROWS), 1, piece.col, piece.color, 0))
+        if piece.col <= 2: #check right
+            moves.update(self._check_eatable_horizontal(col + 1, min(piece.col+6, COLS), 1, piece.row, piece.color, 0))
+            moves.update(self._check_eatable_horizontal(col -1, max(piece.col-6, -1), -1, piece.row, piece.color, 0))
+        else: #check left
+            moves.update(self._check_eatable_horizontal(col -1, max(piece.col-6, -1), -1, piece.row, piece.color, 0))
+            moves.update(self._check_eatable_horizontal(col + 1, min(piece.col+6, COLS), 1, piece.row, piece.color, 0))
+
+        return moves
+
+    def _check_eatable_vertical(self, start, stop, step, col, color, count):
+        moves = {}
+        last = []
+
+        for r in range(start, stop, step):
+            if count > 5:
+                break
+            if start == - 1:
+                if col <= 2:
+                    moves.update(self._check_eatable_horizontal(0, min(col + 6, COLS), 1, col, color, count + 1))
+                else:
+                    moves.update(self._check_eatable_horizontal(COLS, max(col-6,-1), -1, ROWS-col, color, count + 1))
+            elif start == 6:
+                if col <= 2:
+                    moves.update(self._check_eatable_horizontal(0, min(col + 6, COLS), 1, ROWS-col, color, count + 1))
+                else:
+                    moves.update(self._check_eatable_horizontal(COLS, max(col-6,-1), -1, col, color, count + 1))
+            else:
+                current = self.board[r][col]
+                if current != 0 and current.color != color:
+                    if count > 0 and not moves:
+                        moves[(r, col)] = current
+                    else:
+                        break
+                elif current == 0:
+                    last = [(r, col)]
+                    print(last)
+                    print(current)
+                    print(moves)
+
+                    if last:
+                        if last[0][0] == ROWS - 1 and last[0][1] > 2:
+                            moves.update(self._check_eatable_horizontal(COLS, max(last[0][1]-6,-1), -1, col, color, count + 1))
+                        elif last[0][0] == ROWS - 1 and last[0][1] <= 2:
+                            moves.update(self._check_eatable_horizontal(0, min(last[0][1]+6, COLS), 1, last[0][0] - col, color, count + 1))
+                        elif last[0][0] == 0 and last[0][1] > 2:
+                            moves.update(self._check_eatable_horizontal(COLS, max(last[0][1]-6,-1), -1, ROWS - col, color, count + 1))
+                        elif last[0][0] == 0 and last[0][1] <= 2:
+                            moves.update(self._check_eatable_horizontal(0, min(last[0][1]+6, COLS), 1, col, color, count + 1))
+                elif current.color == color:
+                    break
+
+        return moves
+
+    def _check_eatable_horizontal(self, start, stop, step, row, color, count):
+        moves = {}
+        last = []
+
+        for c in range(start, stop, step):
+            if count > 5:
+                break
+            if start == - 1:
+                if row <= 2:
+                    moves.update(self._check_eatable_vertical(0, min(row + 6, ROWS), 1, row, color, count + 1))
+                else:
+                    moves.update(self._check_eatable_vertical(ROWS, max(row - 6, -1), -1, COLS - row, color, count + 1))
+            elif start == 6:
+                if row <= 2:
+                    moves.update(self._check_eatable_vertical(0, min(row + 6, ROWS), 1, COLS - row, color, count + 1))
+                else:
+                    moves.update(self._check_eatable_vertical(ROWS, max(row - 6, -1), -1, row, color, count + 1))
+            else:
+                current = self.board[row][c]
+                if current != 0 and current.color != color:
+                    if count > 0 and not moves:
+                        moves[(row, c)] = current
+                    else:
+                        break
+                elif current == 0:
+                    last = [(row, c)]
+
+                    if last:
+                        if last[0][1] == COLS - 1 and last[0][0] > 2:
+                            moves.update(self._check_eatable_vertical(ROWS, max(last[0][0]-6,-1), -1, row, color, count + 1))
+                        elif last[0][1] == COLS - 1 and last[0][0] <= 2:
+                            moves.update(self._check_eatable_vertical(0, min(last[0][0]+6,ROWS), 1, last[0][1]-row, color, count + 1))
+                        elif last[0][1] == 0 and last[0][0] > 2:
+                            moves.update(self._check_eatable_vertical(ROWS, max(last[0][0]-6,-1), -1, COLS - row, color, count + 1))
+                        elif last[0][1] == 0 and last[0][0] <= 2:
+                            moves.update(self._check_eatable_vertical(0, min(last[0][0]+6,ROWS), 1, row, color, count + 1))
+        return moves
+
+    def _check_moves(self, go_row, go_col):
+        moves = {}
+        valid = []
+
+        for i in range(1):
+            if go_col < 0 or go_col > 5:
+                break
+            if go_row < 0 or go_row > 5:
+                break
+            else:
+                current = self.board[go_row][go_col]
+                if current == 0:
+                    valid = [current]
+                    moves[(go_row, go_col)] = valid
+
+        return moves
 
 
     #RULES:
